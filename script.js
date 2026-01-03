03b5561a31fa7b71def0bfa63b0f7a533efa1506
Kamis <kamillarebeka01@gmail.com>
2026-01-03 16:51:03 -0300

    animação da barra lateral

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x")
+    menuIcon.classList.remove("bx-menu")
     navbar.classList.toggle('active')
 }
 
 document.querySelectorAll('.navbar a').forEach(link => {
     link.onclick = () => {
         navbar.classList.remove('active');
-        menuIcon.classList.remove('bx-x');
+        menuIcon.classList.toggle('bx-menu');
     };
+});
+
+document.addEventListener('click', (e) => {
+    const clickedInsideMenu = navbar.contains(e.target);
+    const clickedMenuIcon = menuIcon.contains(e.target);
+
+    if (!clickedInsideMenu && !clickedMenuIcon) {
+        navbar.classList.remove('active');
+        menuIcon.classList.remove('bx-x');
+        menuIcon.classList.add('bx-menu');
+    }
 });
